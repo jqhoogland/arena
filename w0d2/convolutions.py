ce827714ca822ed76e94ba5bc9949756cb757f1e
Jesse Hoogland <jessequinten@gmail.com>
2022-10-21 11:19:52 +0200

    ADD pad2d

--- w0d2/convolutions.py
+++ w0d2/convolutions.py
@@ -93,12 +93,30 @@ def pad1d(x: t.Tensor, left: int, right: int, pad_value: float) -> t.Tensor:
     '''
     batch_size, n_in_channels, width = x.shape
 
-    x_padded = t.ones((batch_size, n_in_channels, width + left + right)) * pad_value
-    x_padded[..., left:-right] = x[:,:,:]
+    x_padded = t.ones((batch_size, n_in_channels, left + width + right), dtype=x.dtype) * pad_value
+    x_padded[..., left:left + width] = x[:, :, :]
     
     return x_padded
 
 
 utils.test_pad1d(pad1d)
 utils.test_pad1d_multi_channel(pad1d)
+# %%
+
+def pad2d(x: t.Tensor, left: int, right: int, top: int, bottom: int, pad_value: float) -> t.Tensor:
+    '''Return a new tensor with padding applied to the edges.
+
+    x: shape (batch, in_channels, height, width), dtype float32
+
+    Return: shape (batch, in_channels, top + height + bottom, left + width + right)
+    '''
+    batch_size, n_in_channels, height, width = x.shape
+
+    x_padded = t.ones((batch_size, n_in_channels, top + height + bottom, left + width + right), dtype=x.dtype) * pad_value
+    x_padded[..., top:top + height, left:left + width] = x[:, :, :, :]
+    
+    return x_padded
+
+utils.test_pad2d(pad2d)
+utils.test_pad2d_multi_channel(pad2d)
 # %%
