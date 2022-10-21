8d2b328fcdc3e79a719df1e978c8b54f7d856a92
Jesse Hoogland <jessequinten@gmail.com>
2022-10-21 11:50:15 +0200

    ADD conv2d

--- w0d2/convolutions.py
+++ w0d2/convolutions.py
@@ -94,7 +94,7 @@ def pad1d(x: t.Tensor, left: int, right: int, pad_value: float) -> t.Tensor:
     batch_size, n_in_channels, width = x.shape
 
     x_padded = t.ones((batch_size, n_in_channels, left + width + right), dtype=x.dtype) * pad_value
-    x_padded[..., left:left + width] = x[:, :, :]
+    x_padded[..., left:left + width] = x
     
     return x_padded
 
@@ -113,7 +113,7 @@ def pad2d(x: t.Tensor, left: int, right: int, top: int, bottom: int, pad_value: 
     batch_size, n_in_channels, height, width = x.shape
 
     x_padded = t.ones((batch_size, n_in_channels, top + height + bottom, left + width + right), dtype=x.dtype) * pad_value
-    x_padded[..., top:top + height, left:left + width] = x[:, :, :, :]
+    x_padded[..., top:top + height, left:left + width] = x
     
     return x_padded
 
@@ -157,5 +157,66 @@ def conv1d(x, weights, stride: int = 1, padding: int = 0) -> t.Tensor:
 
 
 utils.test_conv1d(conv1d)
+
+# %%
+
+IntOrPair = int | tuple[int, int]
+Pair = tuple[int, int]
+
+def force_pair(v: IntOrPair) -> Pair:
+    '''Convert v to a pair of int, if it isn't already.'''
+    if isinstance(v, tuple):
+        if len(v) != 2:
+            raise ValueError(v)
+        return (int(v[0]), int(v[1]))
+    elif isinstance(v, int):
+        return (v, v)
+    raise ValueError(v)
+
+# %%
+
+def conv2d(x, weights, stride: IntOrPair = 1, padding: IntOrPair = 0) -> t.Tensor:
+    '''Like torch's conv2d using bias=False
+
+    x: shape (batch, in_channels, height, width)
+    weights: shape (out_channels, in_channels, kernel_height, kernel_width)
+
+    Returns: shape (batch, out_channels, output_height, output_width)
+    '''
+    padding_y, padding_x = force_pair(padding)
+    stride_y, stride_x = force_pair(stride)
+
+    batch_size, n_in_channels, height, width = x.shape
+    
+    _, _n_in_channels, kernel_height, kernel_width = weights.shape
+    outer_height = (height + 2 * padding_y - kernel_height) // stride_y + 1 
+    outer_width = (width + 2 * padding_x - kernel_width) // stride_x + 1
+
+    assert n_in_channels == _n_in_channels, \
+        f"The number of in channels must match between `x` and `weights`, received {n_in_channels} and {_n_in_channels}"
+  
+    x_padded = pad2d(x, padding_x, padding_x, padding_y, padding_y, 0.)
+    batch_stride, in_channels_stride, height_stride, width_stride = x_padded.stride() # type: ignore
+    
+    x_view = x_padded.as_strided(size=(
+        batch_size,
+        n_in_channels,
+        outer_height,
+        outer_width,
+        kernel_height,
+        kernel_width
+    ), stride=(
+        batch_stride,
+        in_channels_stride,
+        height_stride * stride_y,
+        width_stride * stride_x,
+        height_stride,
+        width_stride
+    ))
+
+    return einsum("b c_in h_out w_out h_kernel w_kernel, c_out c_in h_kernel w_kernel -> b c_out h_out w_out", x_view, weights)
+
+
+utils.test_conv2d(conv2d)
 
 # %%
