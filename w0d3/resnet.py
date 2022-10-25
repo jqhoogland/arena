6a80992b0991fc1e07ef795d0c1531afd974168d
Jesse Hoogland <jessequinten@gmail.com>
2022-10-25 15:07:24 +0200

    ADD BatchNorm2d module

--- w0d3/resnet.py
+++ w0d3/resnet.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import torch as t
 import torchvision
+from arena.w0d3 import utils
 from PIL import Image
 from plotly.subplots import make_subplots
 from torch import nn
@@ -15,128 +16,6 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from tqdm import tqdm
 
-from arena.w0d3 import utils
-
-# %%
-
-class ConvNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
-        self.relu1 = nn.ReLU() 
-        self.maxpool1 = nn.MaxPool2d(2, stride=2)
-        
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(2, stride=2)
-
-        self.flatten = nn.Flatten()
-        
-        self.linear1 = nn.Linear(3136, 128)
-        self.relu3 = nn.ReLU()
-        self.linear2 = nn.Linear(128, 10)
-        self.softmax = nn.Softmax(dim=-1)
-
-
-    def forward(self, x: t.Tensor) -> t.Tensor:
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.maxpool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.maxpool2(x)
-        x = self.flatten(x)
-        x = self.linear1(x)
-        x = self.relu3(x)
-        x = self.linear2(x)
-        x = self.softmax(x)
-        return x
-
-# %%
-
-model = ConvNet()
-# %%
-
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-])
-
-trainset = datasets.MNIST(root="./data", train=True, transform=transform, download=True)
-trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
-
-testset =  datasets.MNIST(root="./data", train=False, transform=transform, download=True)
-testloader = DataLoader(testset, batch_size=64, shuffle=True)
-
-# %%
-
-device = t.device('cuda:0' if t.cuda.is_available() else 'cpu')
-
-# %%
-
-epochs = 3
-loss_fn = nn.CrossEntropyLoss()
-batch_size = 128
-
-MODEL_FILENAME = "./w1d2_convnet_mnist.pt"
-device = "cuda" if t.cuda.is_available() else "cpu"
-
-def train_convnet(trainloader: DataLoader, epochs: int, loss_fn: Callable) -> list:
-    '''
-    Defines a ConvNet using our previous code, and trains it on the data in trainloader.
-    '''
-
-    model = ConvNet().to(device).train()
-    optimizer = t.optim.Adam(model.parameters())
-    loss_list = []
-    accuracy_list = []
-
-    for epoch in tqdm(range(epochs)):
-
-        for (x, y) in tqdm(trainloader, leave=False):
-
-            x = x.to(device)
-            y = y.to(device)
-
-            y_hat = model(x)
-            loss: t.Tensor = loss_fn(y_hat, y)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-
-            loss_list.append(loss.item())
-
-        accuracy = 0
-
-        for (x, y) in tqdm(testloader, leave=False):
-            x = x.to(device)
-            y = y.to(device)
-
-            y_hat = model(x)
-            accuracy += (y_hat.argmax(dim=1) == y).sum().item()
-
-        accuracy /= len(testloader.dataset)
-        accuracy_list.append(accuracy)
-
-        print(f"Epoch {epoch+1}/{epochs}, train loss is {loss:.6f}, accuracy is {accuracy:.6f}")
-        # TODO: print accuracy!
-
-    print(f"Saving model to: {MODEL_FILENAME}")
-    t.save(model, MODEL_FILENAME)    
-    return loss_list
-
-# %%
-
-loss_list, accuracy_list = train_convnet(trainloader, epochs, loss_fn)
-
-# %%
-
-utils.plot_loss_and_accuracy(loss_list, accuracy_list)
-
-# fig = px.line(y=loss_list, template="simple_white")
-# fig.update_layout(title="Cross entropy loss on MNIST", yaxis_range=[0, max(loss_list)])
-# fig.show()
 # %%
 
 class Sequential(nn.Module):
@@ -187,18 +66,16 @@ class BatchNorm2d(nn.Module):
         '''
         
         if not self.training:
-            x_hat = (x - self.running_mean) / t.sqrt(self.running_var * self.eps)
+            x_hat = (x - self.running_mean.reshape((1, -1, 1, 1))) / t.sqrt(self.running_var + self.eps)
         else:
             batch_mean = x.mean(dim=(0, 2, 3), keepdim=True)
             batch_var = x.var(dim=(0, 2, 3), keepdim=True)
 
-            self.running_mean = self.momentum * self.running_mean + (1 - self.momentum) * batch_mean
-            self.running_var = self.momentum * self.running_var + (1 - self.momentum) * batch_var
+            self.running_mean = (1 - self.momentum) * self.running_mean + self.momentum * batch_mean[0, :, 0, 0]
+            self.running_var = (1 - self.momentum) * self.running_var + self.momentum * batch_var[0, :, 0, 0]
 
-            x_hat = (x - batch_mean) / t.sqrt(batch_var * self.eps)
+            x_hat = (x - batch_mean) / t.sqrt(batch_var + self.eps)
             self.num_batches_tracked += 1
-
-        print(x_hat.shape, self.weight.shape, self.bias.shape)
 
         return x_hat * self.weight.as_strided(size=x_hat.shape, stride=(0, 1, 0, 0)) \
             + self.bias.as_strided(size=x_hat.shape, stride=(0, 1, 0, 0))
