27be8e9ec95283e5b422cadfd7226fa2c4dc758c
callummcdougall <cal.s.mcdougall@gmail.com>
2022-10-31 23:32:47 +0000

    new transformer exercises

--- w2d2/utils.py
+++ w2d2/utils.py
@@ -110,50 +110,34 @@ def plot_gelu(GELU):
     px.line(x=x, y=gelu(x), template="ggplot2").show()
 
 
-def print_param_count(*models, display_df=True, use_state_dict=False):
-    """
-    display_df: bool
-        If true, displays styled dataframe
-        if false, returns dataframe
-
-    use_state_dict: bool
-        If true, uses model.state_dict() to construct dataframe
-            This will include buffers, not just params
-        If false, uses model.named_parameters() to construct dataframe
-            This misses out buffers (more useful for GPT)
-    """
+def print_param_count(*models):
     df_list = []
     gmap_list = []
     for i, model in enumerate(models, start=1):
-        print(f"Model {i}, total params = {sum([param.numel() for name, param in model.named_parameters()])}")
-        iterator = model.state_dict().items() if use_state_dict else model.named_parameters()
+        print(f"Model {i}, total params = {sum([param.numel() for param in model.parameters()])}")
         df = pd.DataFrame([
             {f"name_{i}": name, f"shape_{i}": tuple(param.shape), f"num_params_{i}": param.numel()}
-            for name, param in iterator
+            for name, param in model.named_parameters()
         ]) if (i == 1) else pd.DataFrame([
             {f"num_params_{i}": param.numel(), f"shape_{i}": tuple(param.shape), f"name_{i}": name}
-            for name, param in iterator
+            for name, param in model.named_parameters()
         ])
-        display(df)
         df_list.append(df)
         gmap_list.append(np.log(df[f"num_params_{i}"]))
     df = df_list[0] if len(df_list) == 1 else pd.concat(df_list, axis=1).fillna(0)
     for i in range(1, len(models) + 1):
         df[f"num_params_{i}"] = df[f"num_params_{i}"].astype(int)
+    s = df.style
+    for i in range(1, len(models) + 1):
+        s = s.background_gradient(cmap="viridis", subset=[f"num_params_{i}"], gmap=gmap_list[i-1])
+    with pd.option_context("display.max_rows", 1000):
+        display(s)
     if len(models) > 1:
         param_counts = [df[f"num_params_{i}"].values.tolist() for i in range(1, len(models) + 1)]
         if all([param_counts[0] == param_counts[i] for i in range(1, len(param_counts))]):
             print("All parameter counts match!")
         else:
             print("Parameter counts don't match up exactly.")
-    if display_df:
-        s = df.style
-        for i in range(1, len(models) + 1):
-            s = s.background_gradient(cmap="viridis", subset=[f"num_params_{i}"], gmap=gmap_list[i-1])
-        with pd.option_context("display.max_rows", 1000):
-            display(s)
-    else:
-        return df
 
 def test_load_pretrained_weights(model, tokenizer):
 
@@ -175,10 +159,3 @@ def test_load_pretrained_weights(model, tokenizer):
     print("Your model's top 10 predictions: ", next_tokens)
     assert " Washington" in next_tokens
     assert " Bush" in next_tokens
-
-def test_make_additive_attention_mask(make_additive_attention_mask):
-    from solutions_build_bert import make_additive_attention_mask as make_additive_attention_mask_soln
-    arr = t.randint(low=0, high=2, size=(3, 4))
-    expected = make_additive_attention_mask_soln(arr)
-    actual = make_additive_attention_mask(arr)
-    t.testing.assert_close(expected, actual)
