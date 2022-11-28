8b9cff38200a66aefd80fc9ce5679e805e371126
Jesse Hoogland <jessequinten@gmail.com>
2022-11-28 15:17:36 +0100

    ADD gradient bandit agent

--- cv/rl/bandit.py
+++ cv/rl/bandit.py
@@ -321,3 +321,62 @@ if MAIN:
 
     plot_rewards(all_rewards, names, moving_avg_window=15)
 # %%
+
+class GradientBandit(Agent):
+    def __init__(self, num_arms: int, seed: int, alpha: float):
+        super().__init__(num_arms, seed)
+        self.alpha = alpha
+
+        self.t = 0
+        self.mean_reward = 0
+        self.n = np.zeros(self.num_arms)
+        self.h = np.zeros(self.num_arms)
+
+        self.epsilon = 1e-8 # For numerical stability
+
+    def get_probs(self):
+        return np.exp(self.h) / (np.sum(np.exp(self.h)) + self.epsilon)
+
+    def get_action(self):
+        probs = self.get_probs()
+        return self.rng.choice(self.num_arms, p=probs)
+    
+    def observe(self, action, reward, info):
+        self.t += 1
+        self.n[action] += 1 
+
+        probs = self.get_probs()
+        self.h -= self.alpha * (reward - self.mean_reward) * probs
+        self.h[action] += self.alpha * (reward - self.mean_reward)
+
+        self.mean_reward += (reward - self.mean_reward) / self.t
+
+    def reset(self, seed: int):
+        super().reset(seed)
+
+        self.n = np.zeros(self.num_arms)
+        self.h = np.zeros(self.num_arms)
+
+    def __repr__(self):
+        return f"GradientBandit(alpha={self.alpha})"
+
+
+if MAIN:
+    cheater = CheatyMcCheater(num_arms, 0)
+    reward_averaging = RewardAveraging(num_arms, 0, epsilon=0.1, optimism=0)
+    reward_averaging_optimism = RewardAveraging(num_arms, 0, epsilon=0.1, optimism=5)
+    ucb = UCBActionSelection(num_arms, 0, c=2.0)
+    gradient_bandit = GradientBandit(num_arms, 0, alpha=0.1)
+    random = RandomAgent(num_arms, 0)
+
+    names = []
+    all_rewards = []
+
+    for agent in [cheater, reward_averaging, reward_averaging_optimism, ucb, gradient_bandit, random]:
+        (rewards, num_correct) = test_agent(env, agent, n_runs=N_RUNS)
+        names.append(str(agent))
+        all_rewards.append(rewards)
+
+    plot_rewards(all_rewards, names, moving_avg_window=15)
+
+# %%
